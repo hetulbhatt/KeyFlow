90bcb03a0afc1acbfbf278e12be334e1f385d29e
Hetul Bhatt <hetul21@gmail.com>
2023-06-27 20:24:11 +0530

    to trigger commit for build branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "../include/keyflow/keyhook.hpp"
+#include <stdio.h>
+#include <string_view>
+#include <iostream>
+#include "../include/keyflow/systemtraymanager.hpp"  
 
+//using namespace std::literals;
 int main(int argc, char* argv[]) {
     Logger::initialize();
     Logger::log("OK!\n\n");
+    SystemTrayManager tray{GetConsoleWindow()};
     KeyHook& keyHook = KeyHookSingleton::getInstance();
     FileLoader& fileLoader = keyHook.getKeystrokeHandler().getFileLoader();
     if (argc == 3) {
@@ -11,6 +17,7 @@ int main(int argc, char* argv[]) {
         fileLoader.loadFromFiles();
     }
     keyHook.getKeystrokeHandler().resizeBuffer(fileLoader.getCodeLength());
+
     keyHook.setup_hook();
     Logger::shutdown();
     return 0;
