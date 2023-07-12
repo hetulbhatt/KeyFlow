d1443be08a554520d16ac8adbb0b3993d225f555
Hetul Bhatt <hetul21@gmail.com>
2023-07-12 21:16:26 +0530

    VS formatting change

--- KeyFlow/fileloader.hpp
+++ KeyFlow/fileloader.hpp
@@ -12,206 +12,206 @@
 class FileLoader
 {
 private:
-    Helper helper;
-    std::vector<std::pair<std::vector<int>, std::string>> dollar_shortcuts;
-    std::vector<std::pair<std::vector<int>, std::string>> colon_shortcuts;
-    std::unordered_map<std::string, std::string> dollar_codes;
-    std::unordered_map<std::string, std::string> colon_codes;
-    int code_length = DEFAULT_CODE_LENGTH;
+	Helper helper;
+	std::vector<std::pair<std::vector<int>, std::string>> dollar_shortcuts;
+	std::vector<std::pair<std::vector<int>, std::string>> colon_shortcuts;
+	std::unordered_map<std::string, std::string> dollar_codes;
+	std::unordered_map<std::string, std::string> colon_codes;
+	int code_length = DEFAULT_CODE_LENGTH;
 
 public:
-    FileLoader()
-    {
-        Logger::log("FileLoader()\n");
-    }
+	FileLoader()
+	{
+		Logger::log("FileLoader()\n");
+	}
 
-    std::vector<std::pair<std::vector<int>, std::string>>& getDollarShortcuts()
-    {
-        return this->dollar_shortcuts;
-    }
+	std::vector<std::pair<std::vector<int>, std::string>>& getDollarShortcuts()
+	{
+		return this->dollar_shortcuts;
+	}
 
-    std::vector<std::pair<std::vector<int>, std::string>>& getColonShortcuts()
-    {
-        return this->colon_shortcuts;
-    }
+	std::vector<std::pair<std::vector<int>, std::string>>& getColonShortcuts()
+	{
+		return this->colon_shortcuts;
+	}
 
-    std::unordered_map<std::string, std::string>& getDollarCodes()
-    {
-        return this->dollar_codes;
-    }
+	std::unordered_map<std::string, std::string>& getDollarCodes()
+	{
+		return this->dollar_codes;
+	}
 
-    std::unordered_map<std::string, std::string>& getColonCodes()
-    {
-        return this->colon_codes;
-    }
+	std::unordered_map<std::string, std::string>& getColonCodes()
+	{
+		return this->colon_codes;
+	}
 
-    int getCodeLength()
-    {
-        return code_length;
-    }
+	int getCodeLength()
+	{
+		return code_length;
+	}
 
-    void loadFromFiles() {
-        load_shortcuts("../config/shortcuts.config");
-        load_codes("../config/codes.config");
-    }
+	void loadFromFiles() {
+		load_shortcuts("../config/shortcuts.config");
+		load_codes("../config/codes.config");
+	}
 
-    void loadFromFiles(const std::string& shortcutsFile, const std::string& codesFile) {
-        load_shortcuts(shortcutsFile);
-        load_codes(codesFile);
-    }
+	void loadFromFiles(const std::string& shortcutsFile, const std::string& codesFile) {
+		load_shortcuts(shortcutsFile);
+		load_codes(codesFile);
+	}
 
-    int load_shortcuts(const std::string& filePath)
-    {
-        std::ifstream file(filePath);
-        std::string line;
+	int load_shortcuts(const std::string& filePath)
+	{
+		std::ifstream file(filePath);
+		std::string line;
 
-        while (std::getline(file, line))
-        {
-            if (line.empty() || line[0] == '#')
-            {
-                continue;
-            }
+		while (std::getline(file, line))
+		{
+			if (line.empty() || line[0] == '#')
+			{
+				continue;
+			}
 
-            std::stringstream ss(line);
-            std::string keyCombination, value;
-            bool is_dollar = false, is_colon = false;
-            if (line.find('$') != std::string::npos)
-            {
-                is_dollar = true;
-                std::getline(ss, keyCombination, '$');
-                std::getline(ss, value);
-            }
-            else if (line.find(':') != std::string::npos)
-            {
-                is_colon = true;
-                std::getline(ss, keyCombination, ':');
-                std::getline(ss, value);
-            }
+			std::stringstream ss(line);
+			std::string keyCombination, value;
+			bool is_dollar = false, is_colon = false;
+			if (line.find('$') != std::string::npos)
+			{
+				is_dollar = true;
+				std::getline(ss, keyCombination, '$');
+				std::getline(ss, value);
+			}
+			else if (line.find(':') != std::string::npos)
+			{
+				is_colon = true;
+				std::getline(ss, keyCombination, ':');
+				std::getline(ss, value);
+			}
 
-            keyCombination = helper.trim(keyCombination);
-            value = helper.trim(value);
+			keyCombination = helper.trim(keyCombination);
+			value = helper.trim(value);
 
-            std::vector<std::string> splitStrings = helper.split_string(keyCombination, '+');
-            std::vector<int> splitInts = helper.convert_strings_to_ints(splitStrings);
+			std::vector<std::string> splitStrings = helper.split_string(keyCombination, '+');
+			std::vector<int> splitInts = helper.convert_strings_to_ints(splitStrings);
 
-            if (!splitInts.empty() && !value.empty())
-            {
-                if (is_dollar)
-                {
-                    dollar_shortcuts.push_back(std::make_pair(splitInts, value));
-                }
-                else if (is_colon)
-                {
-                    colon_shortcuts.push_back(std::make_pair(splitInts, value));
-                }
-            }
-        }
+			if (!splitInts.empty() && !value.empty())
+			{
+				if (is_dollar)
+				{
+					dollar_shortcuts.push_back(std::make_pair(splitInts, value));
+				}
+				else if (is_colon)
+				{
+					colon_shortcuts.push_back(std::make_pair(splitInts, value));
+				}
+			}
+		}
 
-        Logger::log("Dollar Shortcuts:\n");
-        for (const auto& entry : dollar_shortcuts)
-        {
-            for (auto& num : entry.first)
-            {
-                Logger::log(std::to_string(num) + ", ");
-            }
-            Logger::log(" : " + entry.second + "\n");
-        }
-        Logger::log("\n");
+		Logger::log("Dollar Shortcuts:\n");
+		for (const auto& entry : dollar_shortcuts)
+		{
+			for (auto& num : entry.first)
+			{
+				Logger::log(std::to_string(num) + ", ");
+			}
+			Logger::log(" : " + entry.second + "\n");
+		}
+		Logger::log("\n");
 
-        Logger::log("Colon Shortcuts:\n");
-        for (const auto& entry : colon_shortcuts)
-        {
-            for (auto& num : entry.first)
-            {
-                Logger::log(std::to_string(num) + ", ");
-            }
-            Logger::log(" : " + entry.second + "\n");
-        }
-        Logger::log("\n");
+		Logger::log("Colon Shortcuts:\n");
+		for (const auto& entry : colon_shortcuts)
+		{
+			for (auto& num : entry.first)
+			{
+				Logger::log(std::to_string(num) + ", ");
+			}
+			Logger::log(" : " + entry.second + "\n");
+		}
+		Logger::log("\n");
 
-        return 0;
-    }
+		return 0;
+	}
 
-    int load_codes(const std::string& filePath)
-    {
-        std::ifstream file(filePath);
-        std::string line;
+	int load_codes(const std::string& filePath)
+	{
+		std::ifstream file(filePath);
+		std::string line;
 
-        while (std::getline(file, line))
-        {
-            if (line.empty() || line[0] == '#')
-                continue;
+		while (std::getline(file, line))
+		{
+			if (line.empty() || line[0] == '#')
+				continue;
 
-            if (helper.starts_with(line, "CODE LENGTH"))
-            {
-                std::stringstream ss(line);
-                std::string code_length_declarator, actual_length;
-                std::getline(ss, code_length_declarator, '=');
-                std::getline(ss, actual_length);
-                try
-                {
-                    code_length = std::stoi(helper.trim(actual_length));
-                }
-                catch (const std::invalid_argument& e)
-                {
-                    code_length = DEFAULT_CODE_LENGTH;
-                }
-                continue;
-            }
+			if (helper.starts_with(line, "CODE LENGTH"))
+			{
+				std::stringstream ss(line);
+				std::string code_length_declarator, actual_length;
+				std::getline(ss, code_length_declarator, '=');
+				std::getline(ss, actual_length);
+				try
+				{
+					code_length = std::stoi(helper.trim(actual_length));
+				}
+				catch (const std::invalid_argument& e)
+				{
+					code_length = DEFAULT_CODE_LENGTH;
+				}
+				continue;
+			}
 
-            std::stringstream ss(line);
-            std::string keyCombination, value;
-            bool is_dollar = false, is_colon = false;
-            if (line.find('$') != std::string::npos)
-            {
-                is_dollar = true;
-                std::getline(ss, keyCombination, '$');
-                std::getline(ss, value);
-            }
-            else if (line.find(':') != std::string::npos)
-            {
-                is_colon = true;
-                std::getline(ss, keyCombination, ':');
-                std::getline(ss, value);
-            }
+			std::stringstream ss(line);
+			std::string keyCombination, value;
+			bool is_dollar = false, is_colon = false;
+			if (line.find('$') != std::string::npos)
+			{
+				is_dollar = true;
+				std::getline(ss, keyCombination, '$');
+				std::getline(ss, value);
+			}
+			else if (line.find(':') != std::string::npos)
+			{
+				is_colon = true;
+				std::getline(ss, keyCombination, ':');
+				std::getline(ss, value);
+			}
 
-            keyCombination = helper.trim(keyCombination);
-            value = helper.trim(value);
+			keyCombination = helper.trim(keyCombination);
+			value = helper.trim(value);
 
-            if (keyCombination.size() != code_length)
-            {
-                continue;
-            }
+			if (keyCombination.size() != code_length)
+			{
+				continue;
+			}
 
-            if (!keyCombination.empty() && !value.empty())
-            {
-                if (is_dollar)
-                {
-                    dollar_codes[keyCombination] = value;
-                }
-                else if (is_colon)
-                {
-                    colon_codes[keyCombination] = value;
-                }
-            }
-        }
+			if (!keyCombination.empty() && !value.empty())
+			{
+				if (is_dollar)
+				{
+					dollar_codes[keyCombination] = value;
+				}
+				else if (is_colon)
+				{
+					colon_codes[keyCombination] = value;
+				}
+			}
+		}
 
-        Logger::log("Dollar Code:\n");
-        for (const auto& entry : dollar_codes)
-        {
-            Logger::log(entry.first + " : " + entry.second + "\n");
-        }
-        Logger::log("\n");
+		Logger::log("Dollar Code:\n");
+		for (const auto& entry : dollar_codes)
+		{
+			Logger::log(entry.first + " : " + entry.second + "\n");
+		}
+		Logger::log("\n");
 
-        Logger::log("Colon Code:\n");
-        for (const auto& entry : colon_codes)
-        {
-            Logger::log(entry.first + " : " + entry.second + "\n");
-        }
-        Logger::log("\n");
+		Logger::log("Colon Code:\n");
+		for (const auto& entry : colon_codes)
+		{
+			Logger::log(entry.first + " : " + entry.second + "\n");
+		}
+		Logger::log("\n");
 
-        return 0;
-    }
+		return 0;
+	}
 };
 
 #endif
--- KeyFlow/logger.hpp
+++ KeyFlow/logger.hpp
@@ -10,42 +10,42 @@
 class Logger
 {
 private:
-    static std::ofstream logFile;
+	static std::ofstream logFile;
 
 public:
-    static void initialize()
-    {
-        std::string dirName = "logs";
-        std::string fileName = "logs/execution.log";
+	static void initialize()
+	{
+		std::string dirName = "logs";
+		std::string fileName = "logs/execution.log";
 
-        // Create the directory if it doesn't exist
-        int result = _mkdir(dirName.c_str());
-        if (result != 0)
-        {
-            if (errno != EEXIST)
-            {
-                std::cerr << "Failed to create the logs directory!" << std::endl;
-            }
-        }
+		// Create the directory if it doesn't exist
+		int result = _mkdir(dirName.c_str());
+		if (result != 0)
+		{
+			if (errno != EEXIST)
+			{
+				std::cerr << "Failed to create the logs directory!" << std::endl;
+			}
+		}
 
-        logFile.open(fileName);
-        if (!logFile.is_open())
-        {
-            std::cerr << "Failed to open the log file for writing!" << std::endl;
-        }
-    }
+		logFile.open(fileName);
+		if (!logFile.is_open())
+		{
+			std::cerr << "Failed to open the log file for writing!" << std::endl;
+		}
+	}
 
-    static void shutdown()
-    {
-        logFile.close();
-    }
+	static void shutdown()
+	{
+		logFile.close();
+	}
 
-    static void log(const std::string& message)
-    {
-        std::cout << message;
-        logFile << message;
-        logFile.flush();
-    }
+	static void log(const std::string& message)
+	{
+		std::cout << message;
+		logFile << message;
+		logFile.flush();
+	}
 };
 
 std::ofstream Logger::logFile; // Definition of the static member
